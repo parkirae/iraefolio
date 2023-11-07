316f42d7df7e4cd674031fd3157c7735604ca087
parkirae <parkirae@kakao.com>
2023-11-07 17:23:48 +0900

    HOTFIX:fire:: CORS 전역 설정으로 변경

---
+++ src/main/java/com/iraefolio/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.iraefolio.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://iraefolio.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true); // 쿠키를 포함시킬 경우 true로 설정
+    }
+}
