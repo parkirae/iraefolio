4f10a4ced7424550c383f7e242125f3b317d9ec1
parkirae <parkirae@kakao.com>
2023-11-08 11:05:32 +0900

    TEST:docs:: cors disable

--- src/main/java/com/iraefolio/config/SecurityConfig.java
+++ src/main/java/com/iraefolio/config/SecurityConfig.java
@@ -42,8 +42,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors()
-                .and()
+                .cors().disable()
                 .formLogin()
                 /* 로그인 페이지 지정 */
                 .loginPage("/login")
